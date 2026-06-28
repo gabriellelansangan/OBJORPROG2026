b5e63c7f96cf11f7742e812fbe37872c0e404f04
Anna Gabrielle Lansangan <annagabriellelansangan@lilac-lieutenant.local>
2026-06-29 01:59:20 +0800

    All done

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import home_view
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
+    path('contact', views.contact_view, name='contact_submit'),
 ]
