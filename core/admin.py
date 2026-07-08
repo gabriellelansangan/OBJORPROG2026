3c8d56749b1046eff4a4497464a55197f568c326
Anna Gabrielle Lansangan <annagabriellelansangan@lilac-lieutenant.local>
2026-07-08 17:46:16 +0800

    Updated index.html and added a new html to showcase project details

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import ContactMessage
+from .models import Project, ContactMessage, PersonalInformation
 
+admin.site.register(Project)
 admin.site.register(ContactMessage)
+admin.site.register(PersonalInformation)
--- core/models.py
+++ core/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
 class Project(models.Model):
-    title = models.CharField(max_length=200)
+    project_name = models.CharField(max_length=200)
     description = models.TextField()
-    technology_used = models.CharField(max_length=100)
-    live_link = models.URLField(blank=True)
+    technology_used = models.CharField(max_length=200)
+    link = models.URLField(blank=True, null=True)
 
     def __str__(self):
-        return self.title
+        return self.project_name
+    
+class PersonalInformation(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100)
+    summary = models.TextField()
+    contact_number = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.TextField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
     
 class ContactMessage(models.Model):
     email = models.EmailField()
