e51addb71e825c35f94583de3a061da0dbe4e3cf
Anna Gabrielle Lansangan <annagabriellelansangan@lilac-lieutenant.local>
2026-08-05 19:42:57 +0800

    Added Working Create Projects, Testimonies, and Contact pages

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('contact', views.contact_view, name='contact_submit'),
-    path('project/<int:pk>/', views.project_detail, name='project_detail')
+    path('project/<int:pk>/', views.project_detail, name='project_detail'),
+    path('projects/add/', views.create_project, name='create_project'),
+    path('contact/', views.contact_inquiry, name='contact'),
+    path('testimonies/', views.TestimonyListView.as_view(), name='testimony_list'),
+    path('testimonies/add/', views.create_testimony, name='create_testimony'),
+    path('testimonies/<int:pk>/', views.testimony_detail, name='testimony_detail'),
 ]
